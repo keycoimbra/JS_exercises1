a483e404fa5b1b64ddf7d62ada9e590823a726af
Keyla Coimbra <keyla_thay@hotmail.com>
2022-07-29 17:35:09 -0300

    exercicio 1 - resolvido

--- ex.1.js
+++ ex.1.js
@@ -1,5 +1,9 @@
-
 // Crie uma função que irá imprimir a palavra "ATENTA"
 
-imprimeAtenta(); // Aqui estamos instânciando, chamando a função que você construiu
-// ATENTA
+/*Aqui estamos instânciando, chamando a função que você construiu
+ATENTA imprimeAtenta();*/
+
+function advice(word){
+    console.log('Ao atravessar a rua fique ' + word)
+}
+advice("atenta!");
