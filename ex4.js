ff1420897df7d286e824f5e74e6b00dc54ab873e
Keyla Coimbra <keyla_thay@hotmail.com>
2022-07-29 18:55:44 -0300

    salvando exercicio

--- ex4.js
+++ ex4.js
@@ -1,6 +1,10 @@
-//Crie uma função que calculo de potencias
-//Pode usar a biblioteca math se quiser
+/*Crie uma função que calculo de potencias''
+Pode usar a biblioteca math se quiser
 
+Aqui estamos instânciando, chamando a função que você construiu
+ potencia(2,2); Tem que imprimir o valor de 4*/
 
-potencia(2,2); // Aqui estamos instânciando, chamando a função que você construiu
-// Tem que imprimir o valor de 4
+ function pot(valor1, valor2) {
+    console.log(valor1 + valor2);
+}
+pot(Math.pow[2,2]);
