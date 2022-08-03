4fc6894bf2415aae16fa8debfd7a36c2a4fdd7f2
Keyla Coimbra <keyla_thay@hotmail.com>
2022-08-02 21:04:49 -0300

    iniciando revisão de JS

---
+++ revisaoJS/exPlataforma.js
@@ -0,0 +1,15 @@
+/* Crie um algoritmo que receba três notas de um aluno, calcule sua média 
+e mostre as seguintes mensagens de acordo com cada situação:
+    - Se a media for igual ou maior que 7 - Aprovado
+    - Se a media for maior e igual a cinco e menor que 7 - Recuperação
+    - Se a media for menor que 5 - Reprovado pegar documentação do readline*/
+
+
+    console.log("boletim");
+    let nota1, nota2, nota3;
+
+    function somaBoletim(x, y, z){
+        let media =0;
+        media = ((x, y, z/)/3)
+        if(media>=7)
+    },
